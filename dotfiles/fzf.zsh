c2e87ea70cbed98213342e30da63bc1bfd470b39
trironkk <trironk@gmail.com>
2017-11-07 00:09:00 +0000

    Generalized dotfiles/fzf.* derivation of /home/trironkk directory.

--- dotfiles/fzf.zsh
+++ dotfiles/fzf.zsh
@@ -1,16 +1,16 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/trironkk/workspace/github.com/junegunn/fzf/bin* ]]; then
-  export PATH="$PATH:/home/trironkk/workspace/github.com/junegunn/fzf/bin"
+if [[ ! "$PATH" == *$HOME/workspace/github.com/junegunn/fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/workspace/github.com/junegunn/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/trironkk/workspace/github.com/junegunn/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/workspace/github.com/junegunn/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/trironkk/workspace/github.com/junegunn/fzf/shell/key-bindings.zsh"
+source "$HOME/workspace/github.com/junegunn/fzf/shell/key-bindings.zsh"
 
 # Enable fzf-tmux integration.
 export FZF_TMUX=1
