b5a1ead769e82ee89af8248ccf7821d5f4123986
TK Kiatkungwanglai <trironkk@google.com>
2025-10-18 09:51:19 -0700

    Blanking nvim configs to start from scratch.

--- stows/nvim/.config/nvim/lua/trironkk/plugins/vimade.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"tadaa/vimade",
-	-- default opts (you can partially set these or configure them however you like)
-	opts = {
-		dim_inactive_plugins = true,
-		dim_inactive_tmux_panes = true,
-		recipe = { "minimalist", { animate = true } },
-	},
-}
